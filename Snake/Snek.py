db05d7a4ec58234a0e6f59d051175bf9cef42dfd
Sai Sasank Vindamuri <imsasankvindamuri@gmail.com>
2024-05-21 16:50:59 +0530

    Made Loss Condt

--- Snake/Snek.py
+++ Snake/Snek.py
@@ -1,42 +1,44 @@
 import os
 import msvcrt as m
 import random as r
-lst = [(4,4)]
+snake = [(4,4)]
 clear = lambda: os.system("cls")
 while True:
     apple = (r.randint(0,8),r.randint(0,8))
-    if apple not in lst:
+    if apple not in snake:
         break
 while True:
-    if apple == lst[0]:
+    if apple == snake[0]:
         while True:
             apple = (r.randint(0,8),r.randint(0,8))
-            if apple not in lst:
+            if apple not in snake:
                 break
     grid = [["□" for i in range(9)] for j in range(9)]
     for i in range(9):
         for j in range(9):
             if (i,j) == apple:
                 grid[i][j] = "◉"
-            if (i,j) in lst:
+            if (i,j) in snake:
                 grid[i][j] = "■"                        
     clear()
     for row in grid:
         print(" ".join(row))
     key = m.getch()
     if key == b'H':
-        lst = [(lst[0][0]-1,lst[0][1])] + lst
-        lst.pop()
+        snake = [(snake[0][0]-1,snake[0][1])] + snake
+        snake.pop()
     if key == b'P':
-        lst = [(lst[0][0]+1,lst[0][1])] + lst
-        lst.pop()
+        snake = [(snake[0][0]+1,snake[0][1])] + snake
+        snake.pop()
     if key == b'K':
-        lst = [(lst[0][0],lst[0][1]-1)] + lst
-        lst.pop()
+        snake = [(snake[0][0],snake[0][1]-1)] + snake
+        snake.pop()
     if key == b'M':
-        lst = [(lst[0][0],lst[0][1]+1)] + lst
-        lst.pop()
+        snake = [(snake[0][0],snake[0][1]+1)] + snake
+        snake.pop()
     if key == b'\x1b':
+        print("GAME OVER!!")
         exit()
-    if apple == lst[0]:
-        lst = [apple] + lst
+    if (snake[0][0] == 9) or (snake[0][0] == -1) or (snake[0][1] == 9) or (snake[0][1] == -1) or (snake[0] in snake[1:]):
+        print("GAME OVER!!!")
+        exit()
